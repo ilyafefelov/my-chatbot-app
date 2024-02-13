834352f1aed33eeb85850df2def8677fc04b1cfa
ilyafefelov <ilyafefelov@gmail.com>
2024-02-13 21:14:30 +0200

    some dsgn 1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-teal-900">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-sky-300 p-8">
+  <div class="flex flex-col items-center justify-center mb-4 rounded-lg">
+      <picture>
+        <source srcset="./assets/1500x500.jpg" media="(min-width: 768px)">
+        <img src="./assets/1500x500.jpg" alt="My Image" class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 h-auto rounded-lg mx-auto">
+      </picture>
+      <h1 class="text-4xl font-bold text-pretty text-pink-100 text-center mt-8">Full Iron Design </h1>
+      <h2 class="text-3xl text-pretty text-pink-100 text-center">
+        <span class="italic text-xl font-thin">w/</span>
+        <span class="text-cyan-900 font-medium"> Illya Fefelov</span>
+      </h2>
+  </div>
+  
+    
     <div class="w-full max-w-md p-4 bg-white bg-opacity-60 shadow-lg rounded-lg">
       <div ref="messageList" class="flex flex-col min-h-48 max-h-[80vh] overflow-y-auto mb-4 p-2 bg-orange-100/50 rounded-lg">
         <transition name="fade">
@@ -12,8 +25,11 @@
             </span>
           </div>
         </transition-group>
-        <div v-if="isLoading" class="text-center mt-2">
-          <span class="text-orange-500">Loading...</span>
+        
+        <div class="text-center mt-2 transition-all duration-300 ease-linear"
+              v-if="isLoading"
+        >
+          <span class="text-orange-500">loading...</span>
         </div>
       </div>
 
@@ -40,15 +56,19 @@ const messageList = ref(null);
 
 const handleSendMessage = async () => {
   if (userInput.value.trim()) {
-    isLoading.value = true;
     const response = await sendMessageToWit(userInput.value);
-    botResponse.value = response?.response?.text;
     messages.value.push({ text: userInput.value, sender: 'user' });
+    
+      isLoading.value = true;
+    
+    botResponse.value = response?.response?.text;
     if (botResponse.value) {
-      messages.value.push({ text: botResponse.value, sender: 'bot' });
+      setTimeout(() => {
+        messages.value.push({ text: botResponse.value, sender: 'bot' });
+      }, 1800); // Add a delay of 1000 milliseconds (1 second)
     }
-    userInput.value = '';
     isLoading.value = false;
+    userInput.value = '';
 
     nextTick(() => {
       if (messageList.value) {
